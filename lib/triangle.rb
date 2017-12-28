325f010c7282b177f42ba7ec9be6aae6dde54528
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-12-28 01:16:02 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,10 @@
 class Triangle
-  attr_accessor :a, :b, :c 
+  attr_accessor :a, :b, :c
 
   def initialize(a, b, c)
-    @a = a 
-    @b = b 
-    @c = c 
+    @a = a
+    @b = b
+    @c = c
   end
 
 
